2437eede5e599cc8a7259877663edb2dbfdd7c7b
AlexGB <82088636+agmb5@users.noreply.github.com>
2024-06-10 18:25:35 +0200

    Add coloring function on script.js per request

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
 const gridElement = document.createElement("div");
 const parentDiv = document.querySelector("#parentDiv");
+const btnCreateGrid = document.querySelector("#gridNumber");
+const btnReset = document.querySelector("#resetGame");
 
+//functions 
 function createGrid(numberOfDivs) {
     numberOfDivs = Math.round(numberOfDivs);
     numberOfDivs = Math.abs(numberOfDivs);
+    if (numberOfDivs>100) {numberOfDivs = 100}
     for (let a = 0; a< numberOfDivs;a++) {
         const a = document.createElement("div");
         parentDiv.appendChild(a)
-        a.style.cssText = "flex-grow:1;flex-basis:auto;flex-wrap:wrap;display:flex"   
+        a.classList.add("biggerChild")   
         for (let i = 0; i < (numberOfDivs); i++) {
             const i = document.createElement("div");
             a.appendChild(i)
-            i.style.cssText = "border:1px solid;flex-grow:1;flex-basis:auto;flex-wrap:wrap"
-            
+            i.classList.add("smallerChild")
         }
     }
 
 }
 
+function deleteGrid() {
+    while(parentDiv.firstChild) {
+        parentDiv.removeChild(parentDiv.firstChild)
+    }
+}
 
+function coloring() {
+    let smallerChild = document.querySelectorAll(".smallerChild");
+    [...smallerChild].forEach((box)=>{
+        box.addEventListener("mouseover",()=>{box.style.backgroundColor = "gray";})
+    })
+}
 
-createGrid(2);
+btnCreateGrid.addEventListener("click", ()=>{
+    let humanInput = prompt("Please specify the grid size (maximum 100)","");
+    deleteGrid()
+    createGrid(humanInput)
+    coloring()
+    
+})
+
+btnReset.addEventListener("click", ()=>{
+    let smallerChild = document.querySelectorAll(".smallerChild");
+    [...smallerChild].forEach((box)=>{
+        box.style.backgroundColor = "white";})
+    })
+
+// Inizialise game
+createGrid(16)
+coloring()
